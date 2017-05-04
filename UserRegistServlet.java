420bd0f6afee13f0d28d99cc5d07b12daf8f0747
Sanfield <www.1042824123@qq.com>
2017-05-04 22:54:01 +0800

    注册servlet

---
+++ UserRegistServlet.java
@@ -0,0 +1,61 @@
+package com.web.shopping.user.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.web.shopping.user.bean.User;
+import com.web.shopping.user.service.UserService;
+
+import net.sf.json.JSONObject;
+
+/**
+ * Servlet implementation class UserRegistServlet
+ */
+@WebServlet("/UserRegistServlet")
+public class UserRegistServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserRegistServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+
+		UserService userservice = new UserService();
+		
+		JSONObject json = userservice.register(request);
+		if(json.getInt("result")>0){
+			request.getRequestDispatcher("Login.jsp").forward(request, response);
+		}else{
+		PrintWriter out= response.getWriter();
+		out.write("<div align='center'><font color='red'>注册失败联系管理员</font></div>");
+		out.flush();
+		out.close();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doGet(request, response);
+	}
+
+}
